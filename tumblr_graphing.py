5e785c37aa4a0cb2725ef2a7f9ce07f09d854635
david <david.jaress@gmail.com>
2015-04-05 01:09:11 -0500

    Fix bs4 parsing

--- tumblr_graphing.py
+++ tumblr_graphing.py
@@ -3,19 +3,39 @@
 import requests, sys, re, sqlite3
 from bs4 import BeautifulSoup
 
-def notes_on_page(soup):
-    notes_on_page = []
-    for reblog in soup.find_all("li", "reblog"):
-        notes_on_page.append(reblog.get_text())
-    return notes_on_page
+def save_notes_on_page(db, soup):
+    cursor = db.cursor()
+    for link in soup.find_all("li", "reblog"):
+        reblogger = link.select(".tumblelog")[0].string
+        try:
+            source = link.select(".source_tumblelog")[0].string
+        except IndexError:
+            source = 'none'
+        cursor.execute('''
+            INSERT INTO notes(reblogger, source)
+            values(?,?)''',(reblogger, source)
+        )
+        print (reblogger, source)
+    db.commit()
+
+def create_db():
+    db = sqlite3.connect('data.sqlite3')
+    cursor = db.cursor()
+    cursor.execute('''
+        CREATE TABLE notes(
+            id INTEGER PRIMARY KEY,
+            reblogger TEXT,
+            source TEXT
+        )
+    ''')
+    return db
 
 def get_all_notes(posturl):
+    db = create_db()
     r = requests.get(posturl)
     soup = BeautifulSoup(r.text)
-    reblogs = []
-
     # get notes from front page
-    reblogs + notes_on_page(soup)
+    front_page_notes = save_notes_on_page(db, soup)
 
     morenotes = soup.find_all("a", "more_notes_link")
     while morenotes:
@@ -23,7 +43,6 @@ def get_all_notes(posturl):
         regex = u"'(/notes/[\w=?/]*)'"
         blogurl = "/".join(posturl.split("/")[0:3])
         notes_url = blogurl + re.search(regex, str(morenotes[0])).group(1)
-        print notes_url
 
         # request and soupify the next page of notes
         r = requests.get(notes_url)
@@ -31,33 +50,8 @@ def get_all_notes(posturl):
         morenotes = soup.find_all("a", "more_notes_link")
 
         # get notes from current page
-        reblogs + notes_on_page(soup)
-
-    print len(reblogs)
-    return reblogs
-
-def write_to_db(reblogs):
-    db = sqlite3.connect('data')
-    cursor = db.cursor()
-    cursor.execute('''
-        CREATE TABLE reblogs(
-            id INTEGER PRIMARY KEY,
-            reblogger TEXT,
-            source TEXT
-        )
-    ''')
-    db.commit()
-
-    for reblog in reblogs:
-        poster = reblog.select(".tumblelog")[0].string
-        source = reblog.select(".source_tumblelog")[0].string
-        cursor.execute('''
-            INSERT INTO reblogs(reblogger, source)
-            VALUES(?,?)''',(poster, source))
-        db.commit()
-    db.close()
+        notes_on_page = save_notes_on_page(db, soup)
 
 if __name__ == "__main__":
     posturl = sys.argv[1]
-    reblogs = get_all_notes(posturl)
-    write_to_db(reblogs)
+    get_all_notes(posturl)
