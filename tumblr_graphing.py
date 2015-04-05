8e4b11916e0f657c9918e53638fe56f698a295f8
david <david.jaress@gmail.com>
2015-04-04 22:21:21 -0500

    Replace write to db method

--- tumblr_graphing.py
+++ tumblr_graphing.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import requests, sys, re
+import requests, sys, re, sqlite3
 from bs4 import BeautifulSoup
-from py2neo import neo4j, node, rel
 
 def notes_on_page(soup):
     notes_on_page = []
@@ -37,29 +36,28 @@ def get_all_notes(posturl):
     print len(reblogs)
     return reblogs
 
-def reblogs_into_db(reblogs):
-    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-    people = graph_db.get_or_create_index(neo4j.Node, "People")
-    p_regex = u"([a-z0-9-]*) posted this"
-    rb_regex = u"([a-z0-9-]*) reblogged this from ([a-z0-9-]*)( and added:(.*)$)?"
+def write_to_db(reblogs):
+    db = sqlite3.connect('data')
+    cursor = db.cursor()
+    cursor.execute('''
+        CREATE TABLE reblogs(
+            id INTEGER PRIMARY KEY,
+            reblogger TEXT,
+            source TEXT
+        )
+    ''')
+    db.commit()
+
     for reblog in reblogs:
-        p_match = re.search(p_regex, reblog)
-        rb_match = re.search(rb_regex, reblog)
-        if p_match:
-            poster = graph_db.get_or_create_indexed_node("People", "name", p_match.group(1), properties={"name": p_match.group(1)})
-            poster.add_labels("poster")
-        elif rb_match:
-            reblogger = graph_db.get_or_create_indexed_node("People", "name", rb_match.group(1), properties={"name": rb_match.group(1)})
-            reblogger.add_labels("reblogger")
-            source = graph_db.get_or_create_indexed_node("People", "name", rb_match.group(2), properties={"name": rb_match.group(2)})
-            source.add_labels("source")
-            if rb_match.group(3):
-                properties = {"comment": rb_match.group(3)}
-            else:
-                properties = {}
-            graph_db.create(rel(reblogger, ("Reblogged_from", properties), source))
+        poster = reblog.select(".tumblelog")[0].string
+        source = reblog.select(".source_tumblelog")[0].string
+        cursor.execute('''
+            INSERT INTO reblogs(reblogger, source)
+            VALUES(?,?)''',(poster, source))
+        db.commit()
+    db.close()
 
 if __name__ == "__main__":
     posturl = sys.argv[1]
     reblogs = get_all_notes(posturl)
-    reblogs_into_db(reblogs)
+    write_to_db(reblogs)
